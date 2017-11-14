3ab652c72c3e1f215eb5595869e6d25db9d6f19a
prometheus <prometheus@qq.com>
2017-11-14 09:51:41 +0800

    update productConsumerModel

--- src/main/java/com/enfo/consumerfinance/model/ProductConsumerModel.java
+++ src/main/java/com/enfo/consumerfinance/model/ProductConsumerModel.java
@@ -4,35 +4,89 @@ import java.math.BigDecimal;
 
 public class ProductConsumerModel {
 
-    private Integer product_id;                                  //产品ID号                                                                
-    private String product_code;                             //产品编号                                                
-    private String product_name;                             //产品名称                                                
-    private String product_jc;                               //产品简称                                              
-    private Integer start_date;                                  //产品起始日期                                              
-    private Integer end_date;                                    //产品终止日期                                            
-    private Integer fact_end_date;                               //产品实际结束日期                                                 
-    private Integer valid_period;                                //产品期限(月)                                                
-    private Integer period_unit;                                 //期限计算单位                                               
-    private String product_status;                           //产品状态                                                  
-    private String product_status_name;                      //产品状态说明                                                       
-    private Integer open_flag;                                   //开放式/封闭式                                             
-    private String open_flag_name;                           //开放式/封闭式说明                                                  
-    private String tg_bank_id;                               //托管账户银行id                                              
-    private String tg_bank_name;                             //托管账户银行名称                                                
-    private String tg_bank_sub_name;                         //托管账户支行名称                                                    
-    private String tg_bank_acct;                             //托管账户号                                                
-    private String tg_acct_name;                             //托管开户名称                                                
-    private Integer Integerrust_flag1;                               //单一集合标志                                                 
-    private String Integerrust_type1;                            //项目类型：资金信托、财产信托                                                 
-    private Integer depart_id;                                   //所属部门                                             
-    private BigDecimal fact_money;                           //实际发行金额                                              
-    private Integer cooperation_mode;                            //合作模式（主被、被动）                                                    
-    private BigDecimal stampdutyrate;                        //印花税率
+    private String book_code;
+    private String product_id;
+    private String product_code;
+    private String product_name;
+    private String product_jc;
+    private String item_code;
+    private String item_id;
+    private String currency_id;
+    private String pre_num;
+    private String pre_money;
+    private String min_money;
+    private String pre_start_date;
+    private String pre_end_date;
+    private String pre_max_rate;
+    private String pre_max_num;
+    private String start_date;
+    private String end_date;
+    private String fact_end_date;
+    private String valid_period;
+    private String period_unit;
+    private String product_status;
+    private String product_status_name;
+    private String admin_manager;
+    private String open_flag;
+    private String open_flag_name;
+    private String tg_bank_id;
+    private String tg_bank_name;
+    private String tg_bank_sub_id;
+    private String tg_bank_sub_name;
+    private String tg_bank_acct;
+    private String tg_acct_name;
+    private String intrust_flag1;
+    private String intrust_type1;
+    private String intrust_type1_name;
+    private String depart_id;
+    private String fact_person_num;
+    private String fact_money;
+    private String total_money;
+    private String total_amount;
+    private String nav_price;
+    private String input_man;
+    private String input_time;
+    private String sub_code;
+    private String sub_code_102;
+    private String sub_code_202;
+    private String current_month;
+    private String last_post_date;
+    private String is_local;
+    private String publish_check_flag;
+    private String check_flag;
+    private String admin_manager2;
+    private String matain_manager;
+    private String original_end_date;
+    private String quality_level;
+    private String quality_level_name;
+    private String natrust_type;
+    private String natrust_type_name;
+    private String fprg_flag;
+    private String fprg_times;
+    private String fprg_start_date;
+    private String fprg_cl_date;
+    private String approval_flag;
+    private String item_origin;
+    private String item_origin_name;
+    private String cash_source;
+    private String cash_source_name;
+    private String extension_day;
+    private String matain_manager2;
+    private String product_new_code;
     
-    public Integer getProduct_id() {
+    private Integer cooperation_mode;   
+    private BigDecimal stampdutyrate;
+    
+    public String getBook_code() {
+        return book_code;
+    }
+    public void setBook_code(String book_code) {
+        this.book_code = book_code;
+    }
+    public String getProduct_id() {
         return product_id;
     }
-    public void setProduct_id(Integer product_id) {
+    public void setProduct_id(String product_id) {
         this.product_id = product_id;
     }
     public String getProduct_code() {
@@ -53,34 +107,94 @@ public class ProductConsumerModel {
     public void setProduct_jc(String product_jc) {
         this.product_jc = product_jc;
     }
-    public Integer getStart_date() {
+    public String getItem_code() {
+        return item_code;
+    }
+    public void setItem_code(String item_code) {
+        this.item_code = item_code;
+    }
+    public String getItem_id() {
+        return item_id;
+    }
+    public void setItem_id(String item_id) {
+        this.item_id = item_id;
+    }
+    public String getCurrency_id() {
+        return currency_id;
+    }
+    public void setCurrency_id(String currency_id) {
+        this.currency_id = currency_id;
+    }
+    public String getPre_num() {
+        return pre_num;
+    }
+    public void setPre_num(String pre_num) {
+        this.pre_num = pre_num;
+    }
+    public String getPre_money() {
+        return pre_money;
+    }
+    public void setPre_money(String pre_money) {
+        this.pre_money = pre_money;
+    }
+    public String getMin_money() {
+        return min_money;
+    }
+    public void setMin_money(String min_money) {
+        this.min_money = min_money;
+    }
+    public String getPre_start_date() {
+        return pre_start_date;
+    }
+    public void setPre_start_date(String pre_start_date) {
+        this.pre_start_date = pre_start_date;
+    }
+    public String getPre_end_date() {
+        return pre_end_date;
+    }
+    public void setPre_end_date(String pre_end_date) {
+        this.pre_end_date = pre_end_date;
+    }
+    public String getPre_max_rate() {
+        return pre_max_rate;
+    }
+    public void setPre_max_rate(String pre_max_rate) {
+        this.pre_max_rate = pre_max_rate;
+    }
+    public String getPre_max_num() {
+        return pre_max_num;
+    }
+    public void setPre_max_num(String pre_max_num) {
+        this.pre_max_num = pre_max_num;
+    }
+    public String getStart_date() {
         return start_date;
     }
-    public void setStart_date(Integer start_date) {
+    public void setStart_date(String start_date) {
         this.start_date = start_date;
     }
-    public Integer getEnd_date() {
+    public String getEnd_date() {
         return end_date;
     }
-    public void setEnd_date(Integer end_date) {
+    public void setEnd_date(String end_date) {
         this.end_date = end_date;
     }
-    public Integer getFact_end_date() {
+    public String getFact_end_date() {
         return fact_end_date;
     }
-    public void setFact_end_date(Integer fact_end_date) {
+    public void setFact_end_date(String fact_end_date) {
         this.fact_end_date = fact_end_date;
     }
-    public Integer getValid_period() {
+    public String getValid_period() {
         return valid_period;
     }
-    public void setValid_period(Integer valid_period) {
+    public void setValid_period(String valid_period) {
         this.valid_period = valid_period;
     }
-    public Integer getPeriod_unit() {
+    public String getPeriod_unit() {
         return period_unit;
     }
-    public void setPeriod_unit(Integer period_unit) {
+    public void setPeriod_unit(String period_unit) {
         this.period_unit = period_unit;
     }
     public String getProduct_status() {
@@ -95,10 +209,16 @@ public class ProductConsumerModel {
     public void setProduct_status_name(String product_status_name) {
         this.product_status_name = product_status_name;
     }
-    public Integer getOpen_flag() {
+    public String getAdmin_manager() {
+        return admin_manager;
+    }
+    public void setAdmin_manager(String admin_manager) {
+        this.admin_manager = admin_manager;
+    }
+    public String getOpen_flag() {
         return open_flag;
     }
-    public void setOpen_flag(Integer open_flag) {
+    public void setOpen_flag(String open_flag) {
         this.open_flag = open_flag;
     }
     public String getOpen_flag_name() {
@@ -119,6 +239,12 @@ public class ProductConsumerModel {
     public void setTg_bank_name(String tg_bank_name) {
         this.tg_bank_name = tg_bank_name;
     }
+    public String getTg_bank_sub_id() {
+        return tg_bank_sub_id;
+    }
+    public void setTg_bank_sub_id(String tg_bank_sub_id) {
+        this.tg_bank_sub_id = tg_bank_sub_id;
+    }
     public String getTg_bank_sub_name() {
         return tg_bank_sub_name;
     }
@@ -137,29 +263,233 @@ public class ProductConsumerModel {
     public void setTg_acct_name(String tg_acct_name) {
         this.tg_acct_name = tg_acct_name;
     }
-    public Integer getIntegerrust_flag1() {
-        return Integerrust_flag1;
+    public String getIntrust_flag1() {
+        return intrust_flag1;
     }
-    public void setIntegerrust_flag1(Integer integerrust_flag1) {
-        Integerrust_flag1 = integerrust_flag1;
+    public void setIntrust_flag1(String intrust_flag1) {
+        this.intrust_flag1 = intrust_flag1;
     }
-    public String getIntegerrust_type1() {
-        return Integerrust_type1;
+    public String getIntrust_type1() {
+        return intrust_type1;
     }
-    public void setIntegerrust_type1(String integerrust_type1) {
-        Integerrust_type1 = integerrust_type1;
+    public void setIntrust_type1(String intrust_type1) {
+        this.intrust_type1 = intrust_type1;
     }
-    public Integer getDepart_id() {
+    public String getIntrust_type1_name() {
+        return intrust_type1_name;
+    }
+    public void setIntrust_type1_name(String intrust_type1_name) {
+        this.intrust_type1_name = intrust_type1_name;
+    }
+    public String getDepart_id() {
         return depart_id;
     }
-    public void setDepart_id(Integer depart_id) {
+    public void setDepart_id(String depart_id) {
         this.depart_id = depart_id;
     }
-    public BigDecimal getFact_money() {
+    public String getFact_person_num() {
+        return fact_person_num;
+    }
+    public void setFact_person_num(String fact_person_num) {
+        this.fact_person_num = fact_person_num;
+    }
+    public String getFact_money() {
         return fact_money;
     }
-    public void setFact_money(BigDecimal fact_money) {
+    public void setFact_money(String fact_money) {
         this.fact_money = fact_money;
+    }
+    public String getTotal_money() {
+        return total_money;
+    }
+    public void setTotal_money(String total_money) {
+        this.total_money = total_money;
+    }
+    public String getTotal_amount() {
+        return total_amount;
+    }
+    public void setTotal_amount(String total_amount) {
+        this.total_amount = total_amount;
+    }
+    public String getNav_price() {
+        return nav_price;
+    }
+    public void setNav_price(String nav_price) {
+        this.nav_price = nav_price;
+    }
+    public String getInput_man() {
+        return input_man;
+    }
+    public void setInput_man(String input_man) {
+        this.input_man = input_man;
+    }
+    public String getInput_time() {
+        return input_time;
+    }
+    public void setInput_time(String input_time) {
+        this.input_time = input_time;
+    }
+    public String getSub_code() {
+        return sub_code;
+    }
+    public void setSub_code(String sub_code) {
+        this.sub_code = sub_code;
+    }
+    public String getSub_code_102() {
+        return sub_code_102;
+    }
+    public void setSub_code_102(String sub_code_102) {
+        this.sub_code_102 = sub_code_102;
+    }
+    public String getSub_code_202() {
+        return sub_code_202;
+    }
+    public void setSub_code_202(String sub_code_202) {
+        this.sub_code_202 = sub_code_202;
+    }
+    public String getCurrent_month() {
+        return current_month;
+    }
+    public void setCurrent_month(String current_month) {
+        this.current_month = current_month;
+    }
+    public String getLast_post_date() {
+        return last_post_date;
+    }
+    public void setLast_post_date(String last_post_date) {
+        this.last_post_date = last_post_date;
+    }
+    public String getIs_local() {
+        return is_local;
+    }
+    public void setIs_local(String is_local) {
+        this.is_local = is_local;
+    }
+    public String getPublish_check_flag() {
+        return publish_check_flag;
+    }
+    public void setPublish_check_flag(String publish_check_flag) {
+        this.publish_check_flag = publish_check_flag;
+    }
+    public String getCheck_flag() {
+        return check_flag;
+    }
+    public void setCheck_flag(String check_flag) {
+        this.check_flag = check_flag;
+    }
+    public String getAdmin_manager2() {
+        return admin_manager2;
+    }
+    public void setAdmin_manager2(String admin_manager2) {
+        this.admin_manager2 = admin_manager2;
+    }
+    public String getMatain_manager() {
+        return matain_manager;
+    }
+    public void setMatain_manager(String matain_manager) {
+        this.matain_manager = matain_manager;
+    }
+    public String getOriginal_end_date() {
+        return original_end_date;
+    }
+    public void setOriginal_end_date(String original_end_date) {
+        this.original_end_date = original_end_date;
+    }
+    public String getQuality_level() {
+        return quality_level;
+    }
+    public void setQuality_level(String quality_level) {
+        this.quality_level = quality_level;
+    }
+    public String getQuality_level_name() {
+        return quality_level_name;
+    }
+    public void setQuality_level_name(String quality_level_name) {
+        this.quality_level_name = quality_level_name;
+    }
+    public String getNatrust_type() {
+        return natrust_type;
+    }
+    public void setNatrust_type(String natrust_type) {
+        this.natrust_type = natrust_type;
+    }
+    public String getNatrust_type_name() {
+        return natrust_type_name;
+    }
+    public void setNatrust_type_name(String natrust_type_name) {
+        this.natrust_type_name = natrust_type_name;
+    }
+    public String getFprg_flag() {
+        return fprg_flag;
+    }
+    public void setFprg_flag(String fprg_flag) {
+        this.fprg_flag = fprg_flag;
+    }
+    public String getFprg_times() {
+        return fprg_times;
+    }
+    public void setFprg_times(String fprg_times) {
+        this.fprg_times = fprg_times;
+    }
+    public String getFprg_start_date() {
+        return fprg_start_date;
+    }
+    public void setFprg_start_date(String fprg_start_date) {
+        this.fprg_start_date = fprg_start_date;
+    }
+    public String getFprg_cl_date() {
+        return fprg_cl_date;
+    }
+    public void setFprg_cl_date(String fprg_cl_date) {
+        this.fprg_cl_date = fprg_cl_date;
+    }
+    public String getApproval_flag() {
+        return approval_flag;
+    }
+    public void setApproval_flag(String approval_flag) {
+        this.approval_flag = approval_flag;
+    }
+    public String getItem_origin() {
+        return item_origin;
+    }
+    public void setItem_origin(String item_origin) {
+        this.item_origin = item_origin;
+    }
+    public String getItem_origin_name() {
+        return item_origin_name;
+    }
+    public void setItem_origin_name(String item_origin_name) {
+        this.item_origin_name = item_origin_name;
+    }
+    public String getCash_source() {
+        return cash_source;
+    }
+    public void setCash_source(String cash_source) {
+        this.cash_source = cash_source;
+    }
+    public String getCash_source_name() {
+        return cash_source_name;
+    }
+    public void setCash_source_name(String cash_source_name) {
+        this.cash_source_name = cash_source_name;
+    }
+    public String getExtension_day() {
+        return extension_day;
+    }
+    public void setExtension_day(String extension_day) {
+        this.extension_day = extension_day;
+    }
+    public String getMatain_manager2() {
+        return matain_manager2;
+    }
+    public void setMatain_manager2(String matain_manager2) {
+        this.matain_manager2 = matain_manager2;
+    }
+    public String getProduct_new_code() {
+        return product_new_code;
+    }
+    public void setProduct_new_code(String product_new_code) {
+        this.product_new_code = product_new_code;
     }
     public Integer getCooperation_mode() {
         return cooperation_mode;
@@ -173,25 +503,8 @@ public class ProductConsumerModel {
     public void setStampdutyrate(BigDecimal stampdutyrate) {
         this.stampdutyrate = stampdutyrate;
     }
-    @Override
-    public String toString() {
-        return "ProductConsumerModel [product_id=" + product_id
-                + ", product_code=" + product_code + ", product_name="
-                + product_name + ", product_jc=" + product_jc + ", start_date="
-                + start_date + ", end_date=" + end_date + ", fact_end_date="
-                + fact_end_date + ", valid_period=" + valid_period
-                + ", period_unit=" + period_unit + ", product_status="
-                + product_status + ", product_status_name="
-                + product_status_name + ", open_flag=" + open_flag
-                + ", open_flag_name=" + open_flag_name + ", tg_bank_id="
-                + tg_bank_id + ", tg_bank_name=" + tg_bank_name
-                + ", tg_bank_sub_name=" + tg_bank_sub_name + ", tg_bank_acct="
-                + tg_bank_acct + ", tg_acct_name=" + tg_acct_name
-                + ", Integerrust_flag1=" + Integerrust_flag1
-                + ", Integerrust_type1=" + Integerrust_type1 + ", depart_id="
-                + depart_id + ", fact_money=" + fact_money
-                + ", cooperation_mode=" + cooperation_mode + ", stampdutyrate="
-                + stampdutyrate + "]";
-    }
+
+    
+    
     
 }
